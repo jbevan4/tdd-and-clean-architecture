923ba78df3b4d627616125ddc619ba5b86c35ec0
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-09-18 14:09:38 +0100

    Tested adding three numbers

--- calculator_test.py
+++ calculator_test.py
@@ -5,3 +5,7 @@ def test_can_add_two_numbers():
   calculator = Calculator()
   assert calculator.add(1, 2) == 3
   assert calculator.add(1, 4) == 5
+
+def test_can_add_three_numbers():
+  calculator = Calculator()
+  assert calculator.add(1, 2, 3) == 6
