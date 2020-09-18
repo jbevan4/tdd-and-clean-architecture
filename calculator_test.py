9a7462ea451a99e33d66407177d8194eb3d6c6f8
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-09-18 14:11:18 +0100

    Added a formatter

--- calculator_test.py
+++ calculator_test.py
@@ -2,10 +2,11 @@ from calculator import Calculator
 
 
 def test_can_add_two_numbers():
-  calculator = Calculator()
-  assert calculator.add(1, 2) == 3
-  assert calculator.add(1, 4) == 5
+    calculator = Calculator()
+    assert calculator.add(1, 2) == 3
+    assert calculator.add(1, 4) == 5
+
 
 def test_can_add_three_numbers():
-  calculator = Calculator()
-  assert calculator.add(1, 2, 3) == 6
+    calculator = Calculator()
+    assert calculator.add(1, 2, 3) == 6
