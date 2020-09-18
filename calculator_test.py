e23f4660667f2c5cea3b082b604dfd3c8a3fa1a9
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-09-18 14:12:59 +0100

    Tested adding multiple numbers

--- calculator_test.py
+++ calculator_test.py
@@ -10,3 +10,8 @@ def test_can_add_two_numbers():
 def test_can_add_three_numbers():
     calculator = Calculator()
     assert calculator.add(1, 2, 3) == 6
+
+
+def test_can_add_many_numbers():
+    calculator = Calculator()
+    assert calculator.add(*range(100)) == 4950
