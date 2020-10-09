7acfc7ac4d0d8d2ed38134da8be1d259febb647d
Joshua-Luke Bevan <joshua.bevan@madetech.com>
2020-10-09 14:08:36 +0100

    Added set up method and mock

--- basic_mock/connection.py
+++ basic_mock/connection.py
@@ -1,3 +1,7 @@
 class ConnectionHandler():
     def __init__(self, data_source):
+        self.data_source = data_source
         data_source.connect()
+
+    def set_up(self):
+        self.data_source.set_up(cache=True, max_connections=256)
--- basic_mock/connection_test.py
+++ basic_mock/connection_test.py
@@ -7,3 +7,10 @@ def test_connect():
     external_obj = Mock()
     ConnectionHandler(external_obj)
     external_obj.connect.assert_called_with()
+
+
+def test_setup():
+    external_obj = Mock()
+    ch = ConnectionHandler(external_obj)
+    ch.set_up()
+    external_obj.set_up.assert_called_with(cache=True, max_connections=256)
